56e1d9e722b7f9f99e90b38c0f07abb1b96ae8ae
natmarangonii <nati.mara2009@gmail.com>
2025-06-10 15:52:04 -0300

    Add files via upload

--- RespostaProva_11.cpp
+++ RespostaProva_11.cpp
@@ -2,117 +2,170 @@
 #include <stdlib.h>
 
 typedef struct {
-    char nome[30];
+    char banda[30];
+    char genero[30];
+    int ano;
     int quantidade;
     float preco;
-} Produto;
+    int id;
+} CD;
 
-Produto produto;
+CD cds[100];
+int numCds = 0;
 
-void cadastroProduto();
-void listarProduto();
-void comprarProduto();
+void cadastrarCd();
+void venderCd();
+void consultarCd();
+void listarCds();
 void finalizaFuncao();
 
-int main(){
+int main() {
     int opcao;
-    
-    do{
-        printf("---- SuperMais ----\n");
-        printf("1- Cadastrar produto\n");
-        printf("2- Listar o produto\n");
-        printf("3- Comprar\n");
+    do {
+        printf("---- Som & Arte ----\n");
+        printf("1- Cadastrar CD\n");
+        printf("2- Vender CD\n");
+        printf("3- Consultar CD\n");
+        printf("4- Listar CDS\n");
         printf("0- Sair\n");
         scanf("%d", &opcao);
-        
-        switch (opcao){
+        getchar();
+        switch (opcao) {
             case 1:
-            cadastroProduto();
+            cadastrarCd();
             break;
             
             case 2:
-            listarProduto();
+            venderCd();
             break;
             
             case 3:
-            comprarProduto();
+            consultarCd();
+            break;
+            
+            case 4:
+            listarCds();
             break;
             
             case 0:
-            system ("clear");
+            system("clear");
             printf("Obrigado! :)\n");
             break;
             
             default:
-            system ("clear");
+            system("clear");
             printf("Opção inválida!\n");
             break;
         }
-        
     } while (opcao != 0);
-    
     return 0;
-}
-
-void cadastroProduto() {
-    system ("clear");
-    printf("---- Cadastro do Produto ----\n");
-    printf("Nome: ");
-    scanf(" %[^\n]", produto.nome);
-    
-    printf("Preço: ");
-    scanf("%f", &produto.preco);
-    
-    printf("Quantidade: ");
-    scanf("%d", &produto.quantidade);
-    
-    finalizaFuncao();
-}
-
-void listarProduto() {
-    system ("clear");
-    printf("---- Informações do Produto ----\n");
-    printf("Nome: %s\n", produto.nome);
-    printf("Preço: %.2f\n", produto.preco);
-    printf("Quantidade: %d\n", produto.quantidade);
-    finalizaFuncao();
-}
-
-void comprarProduto() {
-    int quantCompra;
-    float valorCompra;
-    system ("clear");
-    
-        printf("---- Comprar Produto ----\n");
-        printf("Disponivel:\n");
-        printf("%s\n", produto.nome);
-        printf("Preço do produto: %.2f\n", produto.preco);
-        printf("Quantidade disponivel: %d\n", produto.quantidade);
-        printf("Quantidade que deseja comprar de %s:\n", produto.nome);
-        scanf("%d", &quantCompra);
-        
-        valorCompra = quantCompra * produto.preco;
-        
-        if (quantCompra <= produto.quantidade) {
-            system("clear");
-            printf("Compra aprovada!\n");
-            printf("O valor da sua compra é de: %.2f\n", valorCompra);
-            
-            produto.quantidade -= quantCompra;
-            
-            printf("Quantidade restante: %d\n", produto.quantidade);
-        } 
-        else {
-            system("clear");
-            printf("Compra não autorizada! Quantidade indisponivel.\n");
-        }
-    
-    finalizaFuncao();
 }
 
 void finalizaFuncao() {
     printf("Enter para continuar...");
-    getchar();   
     getchar();
     system("clear");
+}
+
+void cadastrarCd() {
+    system("clear");
+    printf("---- Cadastrar CD ----\n");
+    printf("ID do CD: ");
+    scanf("%d", &cds[numCds].id);
+    getchar(); 
+    printf("Banda: ");
+    scanf(" %[^\n]", cds[numCds].banda);
+    printf("Ano: ");
+    scanf("%d", &cds[numCds].ano);
+    getchar();
+    printf("Gênero: ");
+    scanf(" %[^\n]", cds[numCds].genero);
+    printf("Quantidade: ");
+    scanf("%d", &cds[numCds].quantidade);
+    printf("Preço: ");
+    scanf("%f", &cds[numCds].preco);
+    getchar(); 
+    
+    numCds++;
+    
+    finalizaFuncao();
+}
+
+void venderCd() {
+    int id = 0;
+    int quantidade;
+    system("clear");
+    printf("---- Vender CD ----\n");
+    printf("ID do CD: ");
+    scanf("%d", &id);
+    getchar();
+    int encontrado = 0;
+    for (int i = 0; i < numCds; i++) {
+        if (cds[i].id == id) {
+            printf("Quantidade: ");
+            scanf("%d", &quantidade);
+            getchar();
+            if (cds[i].quantidade >= quantidade) {
+                cds[i].quantidade -= quantidade;
+                printf("Venda realizada com sucesso!\n");
+                printf("Valor total da compra: %.2f\n", quantidade * cds[i].preco);
+            } else {
+                printf("Quantidade indisponivel no estoque!\n");
+            }
+            encontrado = 1;
+            break;
+        }
+    }
+    if (!encontrado) {
+        printf("CD não encontrado!\n");
+    }
+    finalizaFuncao();
+}
+
+void consultarCd() {
+    int id;
+    system("clear");
+    printf("---- Consultar CD ----\n");
+    printf("ID do CD: ");
+    scanf("%d", &id);
+    
+    getchar();
+    
+    int encontrado = 0;
+    for (int i = 0; i < numCds; i++) {
+        if (cds[i].id == id) {
+            printf("Banda: %s\n", cds[i].banda);
+            printf("Ano: %d\n", cds[i].ano);
+            printf("Gênero: %s\n", cds[i].genero);
+            printf("Quantidade: %d\n", cds[i].quantidade);
+            printf("Preço: %.2f\n", cds[i].preco);
+            encontrado = 1;
+            break;
+        }
+    }
+    if (!encontrado) {
+        printf("CD não encontrado!\n");
+    }
+    finalizaFuncao();
+}
+
+void listarCds() {
+    system("clear");
+    printf("---- Lista de CDs ----\n");
+    if (numCds == 0) {
+        printf("Nenhum CD cadastrado.\n");
+    } 
+    else {
+        
+        for (int i = 0; i < numCds; i++) {
+            printf("ID: %d\n", cds[i].id);
+            printf("Banda: %s\n", cds[i].banda);
+            printf("Ano: %d\n", cds[i].ano);
+            printf("Gênero: %s\n", cds[i].genero);
+            printf("Quantidade: %d\n", cds[i].quantidade);
+            printf("Preço: %.2f\n", cds[i].preco);
+            printf("-------------------------\n");
+        }
+    }
+    finalizaFuncao();
 }
